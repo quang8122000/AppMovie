e395d8262bfea9bd576f5388cd298e7841795859
quang8122000 <bestxd2k@gmail.com>
2021-04-23 14:02:52 +0700

    new version xxx

--- src/redux/movie/epic.ts
+++ src/redux/movie/epic.ts
@@ -4,31 +4,9 @@ import {systemAction} from '../systems/action';
 import {ofType} from 'redux-observable';
 
 import {catchError, mergeMap} from 'rxjs/operators';
-import {act} from 'react-test-renderer';
 import moment from 'moment';
 import {from} from 'rxjs';
 
-export const getALLMoviess = ($action: any) => {
-  return $action.pipe(
-    ofType(types.GET_ALL_MOVIE),
-    mergeMap((act: any) => {
-      console.log('payload', act.payload);
-      return $axios.api
-        .get(
-          `/3/discover/movie?api_key=2c4916f2a93252ac7140372c475509c6&primary_release_year=${
-            act?.payload?.year || moment().format('YYYY')
-          }&sort_by=revenue.desc&page=${act?.payload?.page || 1}`,
-        )
-        .then((rs: any) => {
-          const {data} = rs;
-          return homeAction.getALLMoviesSuccess(data);
-        })
-        .catch((err: any) => {
-          return homeAction.getALLMoviesFail(err);
-        });
-    }),
-  );
-};
 export const getALLMovies = ($action: any) => {
   return $action.pipe(
     ofType(types.GET_ALL_MOVIE),
